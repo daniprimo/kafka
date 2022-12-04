424305dcdfcd6e2ca098963e92344fca88ec7fc0
DLLM <DLLM@gft.com>
2022-12-03 21:30:04 -0300

    Suindo commit dos primeiros passos com kafka

---
+++ src/main/java/br/com/daniel/KafkaService.java
@@ -0,0 +1,11 @@
+package br.com.daniel;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Collections;
+
+public class KafkaService {
+
+}
